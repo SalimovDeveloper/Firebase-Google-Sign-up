4077414b3aa12865096b13f0803c34d15de6798b
salimovdeveloper <salimovdeveloper@gmail.com>
2022-12-08 14:35:49 -0600

    commit 2

--- app/src/main/java/uz/salimovdeveloper/firebasegooglesignup/MainActivity.kt
+++ app/src/main/java/uz/salimovdeveloper/firebasegooglesignup/MainActivity.kt
@@ -2,10 +2,18 @@ package uz.salimovdeveloper.firebasegooglesignup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+import uz.salimovdeveloper.firebasegooglesignup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient:GoogleSignInClient
+    var RC_SIGN_IN = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
